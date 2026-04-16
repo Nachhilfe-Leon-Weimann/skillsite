527bd8aa18ad2eace30dbfb7a00002c08ef6437e
Leon Weimann <coding@leonweimann.de>
2026-04-16 14:12:59 +0200

    feat: add cta-section on homeplage (#3)

---
+++ src/app/_home/cta-section.tsx
@@ -0,0 +1,31 @@
+import { Container } from "@/components/container";
+import { Section } from "@/components/section";
+import { Button } from "@/components/ui/button";
+import { GradientBackground } from "@/components/ui/gradient-background";
+import { H1, H4, Muted } from "@/components/ui/typography";
+import { routes } from "@/lib/routes";
+import Link from "next/link";
+
+export function CTASection() {
+  return (
+    <GradientBackground className="h-full">
+      <Section className="h-full">
+        <Container className="flex flex-col items-center text-center gap-12">
+          <H1>Starte jetzt mit smarter Nachhilfe</H1>
+
+          <H4 className="max-w-xl">
+            Mehr Verständnis, mehr Selbstbewusstsein, weniger Lernstress.
+          </H4>
+
+          <div className="flex flex-col items-center gap-4">
+            <Button asChild>
+              <Link href={routes.contact}>Jetzt durchstarten</Link>
+            </Button>
+
+            <Muted>Sichere dir dein unverbindliches Kennenlerngespräch</Muted>
+          </div>
+        </Container>
+      </Section>
+    </GradientBackground>
+  );
+}
