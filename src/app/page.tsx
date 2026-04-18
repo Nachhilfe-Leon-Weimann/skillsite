681ceff5c29e6688065cd527806df1d4c4c3ec24
Leon Weimann <coding@leonweimann.de>
2026-04-18 17:01:46 +0200

    feat: show brief benefits on homepage (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { BriefBenefitsSection } from "@/app/_home/brief-benefits-section";
 import { CTASection } from "@/app/_home/cta-section";
 import { HeroSection } from "@/app/_home/hero-section";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div className="h-full">
       <HeroSection />
+      <Separator />
+      <BriefBenefitsSection />
+      <Separator />
       <CTASection />
     </div>
   );
