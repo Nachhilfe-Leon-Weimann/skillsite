262251b10e20cdee8b36e75dcdeb5e04cb4aabb3
Leon Weimann <coding@leonweimann.de>
2026-04-19 22:56:17 +0200

    ref: use cta component for cta section on homepage (#3)

--- src/app/_home/cta-section.tsx
+++ src/app/_home/cta-section.tsx
@@ -1,19 +1,22 @@
+import { CTA, CTAActions, CTAHeader } from "@/components/blocks/cta";
 import { Section } from "@/components/layout/section";
 import { ContactAction } from "@/components/shared/contact-action";
 import { H1, H4 } from "@/components/ui/typography";
 
 export function CTASection() {
   return (
-    <Section
-      gradient="top"
-      containerClassName="items-center text-center gap-12"
-      offsetFooter
-    >
-      <H1>Starte jetzt mit smarter Nachhilfe</H1>
-      <H4 className="max-w-xl">
-        Mehr Verständnis, mehr Selbstbewusstsein, weniger Lernstress.
-      </H4>
-      <ContactAction />
+    <Section gradient="top" offsetFooter>
+      <CTA className="max-w-4xl gap-12">
+        <CTAHeader className="gap-12">
+          <H1>Starte jetzt mit smarter Nachhilfe</H1>
+          <H4 className="max-w-xl">
+            Mehr Verständnis, mehr Selbstbewusstsein, weniger Lernstress.
+          </H4>
+        </CTAHeader>
+        <CTAActions>
+          <ContactAction />
+        </CTAActions>
+      </CTA>
     </Section>
   );
 }
