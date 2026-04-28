78c6b8bd6fe45425b52c74ae457b56bd6d19354e
Leon Weimann <leonweimann@icloud.com>
2026-04-28 23:59:13 +0200

    ref(nav): simplify subjects navigation (#4)

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -1,4 +1,4 @@
-import { LucideIcon, Calculator, Atom, MessageSquareCode } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import { routes } from "@/lib/routes";
 
 export type NavItem = {
@@ -15,26 +15,6 @@ export const navItems: NavItem[] = [
   {
     label: "Fächer",
     href: routes.subjects,
-    children: [
-      // TODO: refactor to use subjectDetails
-      {
-        label: "Mathematik",
-        icon: Calculator,
-        href: routes.maths,
-        description: "Mathematik einfach erklärt.",
-      },
-      {
-        label: "Informatik",
-        icon: MessageSquareCode,
-        href: routes.computer_science,
-      },
-      {
-        label: "Physik",
-        icon: Atom,
-        href: routes.physics,
-        tag: "Neu",
-      },
-    ],
   },
   {
     label: "Details",
